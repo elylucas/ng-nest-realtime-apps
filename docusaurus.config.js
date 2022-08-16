0fd19b4e7a30ad7143430d3ecb72b2a517c8450f
Ely Lucas <ely@meta-tek.net>
2022-08-16 06:55:15 -0600

    action fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,12 +4,14 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+console.log(process.env);
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Realtime Apps with Angular & NestJS',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: process.env.GH_ACTION ? '/ng-nest-realtime-apps/' : '/',
+  baseUrl: process.env.GUIDE ? '/ng-nest-realtime-apps/' : '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
-  
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -34,13 +36,13 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/'
+          routeBasePath: '/',
         },
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        sitemap: false
+        sitemap: false,
       }),
     ],
   ],
