506aeda1ee1a7a26c46804ccf0fa71a799268bb0
Ely Lucas <ely@meta-tek.net>
2022-08-21 16:31:06 -0600

    adding ngoninit to app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   connected = true;
   message = '';
   user = '';
 
   constructor() {}
+
+  ngOnInit(): void {}
 
   connect() {}
 
