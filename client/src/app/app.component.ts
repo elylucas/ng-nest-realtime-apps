6e7e61822405338cad0fe27ce49855a2f3c322a7
Ely Lucas <ely@meta-tek.net>
2022-08-21 13:15:33 -0600

    lab1 complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  message = '';
+  returnedResponses: string[] = [];
+  client = io('http://localhost:3000');
 
+  constructor() {
+
+  }
+
+  sendMessage() {
+    this.client.emit('message', this.message, (msg: string) => {
+      this.returnedResponses.push(msg)
+    });
+  }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EchoGateway } from './echo.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EchoGateway],
 })
 export class AppModule {}
