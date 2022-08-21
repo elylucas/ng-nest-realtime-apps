e43d2ad56344cefaec2f110882c47a63a6282b8e
Ely Lucas <ely@meta-tek.net>
2022-08-21 14:32:37 -0600

    lab2 starting point

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  connected = true;
+  message = '';
+  user = '';
 
+  connect() {
+
+  }
+
+  disconnect() {
+
+  }
+
+  sendMessage() {
+
+  }
 }
